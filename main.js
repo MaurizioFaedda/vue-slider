6a88fc7eedc942b0a60965c9210ad89eb656d55c
Maurizio <maurizio.faedda1@gmail.com>
2020-11-09 14:54:55 +0100

    struttura html e data

---
+++ main.js
@@ -0,0 +1,18 @@
+var app = new App ({
+        el: '#root',
+
+        data: {
+            imgs: [
+                images/natura1.jpg,
+                images/natura2.jpg,
+                images/natura3.jpg,
+                images/natura4.jpg,
+                images/natura5.jpg,
+                
+            ]
+        },
+
+        methods: {
+
+        }
+})
